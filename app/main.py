c8fa9fae03c77fa3223c65931c3c1f408c8117b9
Denys Herasymchuk <herasymchukdenyss@gmail.com>
2025-12-06 20:23:15 +0100

    income has been rounded

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ class CarWashStation:
             income += self.calculate_washing_price(car)
             self.wash_single_car(car)
 
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         pure_cost = self.clean_power - car.clean_mark
